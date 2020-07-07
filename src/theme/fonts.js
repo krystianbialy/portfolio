cd2df5b02bf795858d98133d83ca9a54dd1403ff
krystianbialy <krystian.bialy@onet.pl>
2020-07-07 22:23:10 +0200

    fonts.js file fix

--- src/theme/fonts.js
+++ src/theme/fonts.js
@@ -1,31 +1,40 @@
+import RussoOneRegularWoff2 from '../fonts/RussoOne-Regular/RussoOne-Regular.woff2';
+import RussoOneRegularWoff from '../fonts/RussoOne-Regular/RussoOne-Regular.woff';
+
+import MeriendaOneRegularWoff2 from '../fonts/MeriendaOne-Regular/MeriendaOne-Regular.woff2';
+import MeriendaOneRegularWoff from '../fonts/MeriendaOne-Regular/MeriendaOne-Regular.woff';
+
+import RobotoRegularWoff2 from '../fonts/Roboto-Regular/Roboto-Regular.woff2';
+import RobotoRegularWoff from '../fonts/Roboto-Regular/Roboto-Regular.woff';
+
 export const Fonts = `
-/* russo-one-regular - latin-ext_latin */
-@font-face {
-  font-family: 'Russo One';
-  font-style: normal;
-  font-weight: 400;
-  src: local('Russo One'), local('RussoOne-Regular'),
-       url(${require('../fonts/RussoOne-Regular/RussoOne-Regular.woff2')}) format('woff2'),
-       url(${require('../fonts/RussoOne-Regular/RussoOne-Regular.woff')}) format('woff');
-}
+  /* russo-one-regular - latin-ext_latin */
+  @font-face {
+    font-family: 'Russo One';
+    font-style: normal;
+    font-weight: 400;
+    src: local('Russo One'), local('RussoOne-Regular'),
+      url(${RussoOneRegularWoff2}) format('woff2'), 
+      url(${RussoOneRegularWoff}) format('woff');
+  }
 
-/* merienda-one-regular - latin */
-@font-face {
-  font-family: 'Merienda One';
-  font-style: normal;
-  font-weight: 400;
-  src: local('Merienda One'), local('MeriendaOne-Regular'),
-       url(${require('../fonts/MeriendaOne-Regular/MeriendaOne-Regular.woff2')}) format('woff2'),
-       url(${require('../fonts/MeriendaOne-Regular/MeriendaOne-Regular.woff')}) format('woff');
-}
+  /* merienda-one-regular - latin */
+  @font-face {
+    font-family: 'Merienda One';
+    font-style: normal;
+    font-weight: 400;
+    src: local('Merienda One'), local('MeriendaOne-Regular'),
+      url(${MeriendaOneRegularWoff2}) format('woff2'),
+      url(${MeriendaOneRegularWoff}) format('woff');
+  }
 
-/* roboto-regular - latin-ext_latin */
-@font-face {
-  font-family: 'Roboto';
-  font-style: normal;
-  font-weight: 400;
-  src: local('Roboto'), local('Roboto-Regular'),
-       url(${require('../fonts/Roboto-Regular/Roboto-Regular.woff2')}) format('woff2'),
-       url(${require('../fonts/Roboto-Regular/Roboto-Regular.woff')}) format('woff');
-}
+  /* roboto-regular - latin-ext_latin */
+  @font-face {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    src: local('Roboto'), local('Roboto-Regular'),
+      url(${RobotoRegularWoff2}) format('woff2'),
+      url(${RobotoRegularWoff}) format('woff');
+  }
 `;
